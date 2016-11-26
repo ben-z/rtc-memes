9e8765ec18ad9030d899aa7cfc207e2dd0423179
David Rusu <davidrusu.me@gmail.com>
2016-11-26 15:44:07 -0500

    canvas to draw

--- web/main.js
+++ web/main.js
@@ -1,20 +1,52 @@
 // generate a meme and store in LocalStorage
 
-let data = "Yoooo";
+var canvas = document.getElementById("meme-canvas");
+var ctx = canvas.getContext("2d");
 
-function guid() {
-  function s4() {
-    return Math.floor((1 + Math.random()) * 0x10000)
-      .toString(16)
-      .substring(1);
+var flag = 0;
+canvas.addEventListener("mousedown", function(){
+  flag = 1;
+}, false);
+canvas.addEventListener("mousemove", function(e){
+  if (flag === 0) {
+    return;
   }
-  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
-    s4() + '-' + s4() + s4() + s4();
+  
+  var mX = e.clientX;
+  var mY = e.clientY;
+
+  
+  ctx.fillStyle = "green";
+  ctx.fillRect(mX, mY, 10, 10);
+}, false);
+
+canvas.addEventListener("mouseup", function(){
+  flag = 0;
+}, false);
+
+
+document.getElementById('meme-btn').onclick = create_meme;
+
+function create_meme() {
+  let data = canvas.toDataURL();
+
+  function guid() {
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+      s4() + '-' + s4() + s4() + s4();
+  }
+
+  let uuid = guid();
+  localStorage.setItem("rtc-meme-" + uuid, data);
+
+  let url = window.location.protocol + '//' + window.location.host + "/viewmeme.html?meme_uuid=" + uuid;
+
+  let meme_link = document.getElementById('meme-url');
+  meme_link.href = url;
+  meme_link.innerHTML = url;
+  console.log(url);
 }
-
-let uuid = guid();
-localStorage.setItem("rtc-meme-" + uuid, data);
-
-let url = window.location.protocol + '//' + window.location.host + "/viewmeme.html?meme_uuid=" + uuid;
-
-console.log(url);
