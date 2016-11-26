d57f63820dba346112432250a8cd07aa77c11175
Ben Zhang <benzhangniu@gmail.com>
2016-11-26 12:37:44 -0500

    switchboard example

--- web/main.js
+++ web/main.js
@@ -1,6 +1,29 @@
-var quickconnect = require('rtc-quickconnect');
+// rtc-quickconnect requires a signalling server location and a room name.
+var quickConnectMod = require('rtc-quickconnect');
+var quickConnectObj = quickConnectMod('//switchboard.rtc.io', { room: 'rtcio-text-demo' });
 
-quickconnect('https://switchboard.rtc.io/', { room: 'qc-simple-demo' })
-  .on('call:started', function(id, pc, data) {
-    console.log('we have a new connection to: ' + id);
-  });
+// Create the text area for chatting
+var messageWindow = document.createElement('textarea');
+messageWindow.rows = 20;
+messageWindow.cols = 80;
+
+var bodyElement = document.getElementsByTagName('body')[0];
+bodyElement.appendChild(messageWindow);
+
+// Create a data channel and bind to it's events
+quickConnectObj.createDataChannel('shared-text');
+quickConnectObj.on('channel:opened:shared-text', function (id, dataChannel) {
+  bindDataEvents(dataChannel);
+});
+
+function bindDataEvents(channel) {
+  // Receive message
+  channel.onmessage = function (evt) {
+      messageWindow.value = evt.data;
+  };
+
+  // Send message
+  messageWindow.onkeyup = function (evt) {
+      channel.send(this.value);
+  };
+}
