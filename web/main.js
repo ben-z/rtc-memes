415764947c577560bc156fc185176fd3f8602df5
David Rusu <davidrusu.me@gmail.com>
2016-11-26 12:51:17 -0500

    rtc is working

--- web/main.js
+++ web/main.js
@@ -1,7 +1,11 @@
 // rtc-quickconnect requires a signalling server location and a room name.
 var quickConnectMod = require('rtc-quickconnect');
-var quickConnectObj = quickConnectMod('//switchboard.rtc.io', { room: 'rtcio-text-demo' });
+var quickConnectObj = quickConnectMod('https://switchboard.rtc.io/', { room: 'unicorn-lsdjfflajsdflkajdsklfjalsdj' })
+    .on('call:started', function(id, pc, data) {
+      console.log('we have a new connection to: ' + id);
+    });;
 
+console.log("before messageWindow");
 // Create the text area for chatting
 var messageWindow = document.createElement('textarea');
 messageWindow.rows = 20;
@@ -10,16 +14,20 @@ messageWindow.cols = 80;
 var bodyElement = document.getElementsByTagName('body')[0];
 bodyElement.appendChild(messageWindow);
 
+console.log("after messageWindow");
+
 // Create a data channel and bind to it's events
 quickConnectObj.createDataChannel('shared-text');
 quickConnectObj.on('channel:opened:shared-text', function (id, dataChannel) {
   bindDataEvents(dataChannel);
 });
 
+console.log("created data channels");
 function bindDataEvents(channel) {
   // Receive message
   channel.onmessage = function (evt) {
-      messageWindow.value = evt.data;
+    console.log('evt', evt.data);
+    messageWindow.value = evt.data;
   };
 
   // Send message
