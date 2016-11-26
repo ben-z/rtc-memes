f49d4e6bc76138bbe4d761901af956dcc069647c
Ben Zhang <benzhangniu@gmail.com>
2016-11-26 18:06:38 -0500

    fix typo

--- viewmeme-bundle.js
+++ viewmeme-bundle.js
@@ -42891,7 +42891,7 @@ let meme_base64 = localStorage.getItem('rtc-meme-' + meme_uuid);
 if (meme_base64) {
   renderImage();
   localStorage.removeItem('rtc-meme-' + meme_uuid);
-  console.log('removed rtc-meme-'+meme_uid);
+  console.log('removed rtc-meme-'+meme_uuid);
 }
 
 // WebRTC
